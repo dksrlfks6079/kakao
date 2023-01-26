32bdb93569031e48dd6947d2a5262bf6adde4276
Akr <dksrlfks6079@naver.com>
2023-01-26 15:05:48 +0900

    hhh

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const detail_boxs = document.querySelectorAll(".detail_box")
-detail_boxs.forEach((item) => {
-    item.addEventListener("click", function () {
-        this.classList.toggle("active")
-    })
-})
+// const detail_boxs = document.querySelectorAll(".detail_box")
+// detail_boxs.forEach((item) => {
+//     item.addEventListener("click", function () {
+//         this.classList.toggle("active")
+//     })
+// })
